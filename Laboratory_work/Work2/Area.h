41b362104b739b3e4bcacf4c9cdfff33eaff5685
SuvStreet <suvstreet@gmail.com>
2017-06-07 15:46:37 +0300

    Update Area.h

--- Laboratory_work/Work2/Area.h
+++ Laboratory_work/Work2/Area.h
@@ -1,1 +1,36 @@
-test
+#pragma once
+
+#define PI 3.1415926535897932384626433832795
+#include<iostream>
+#include<cmath>
+
+class Area
+{
+	static int counter;
+	int sideA, sideB, height, diagonalA, diagonalB;
+	double angle;
+public:
+	Area() = default;
+
+	void setsideA(const int);
+	void setsideB(const int);
+	void setheight(const int);
+	void setdiagonalA(const int);
+	void setdiagonalB(const int);
+	void setangle(const double);
+
+	static int getRectangleArea(int, int);
+	static int getRectangleArea(int);
+
+	static int getParallelogramArea(int, int);
+	static double getParallelogramArea(int, int, double);
+
+	static double getRhombusArea(int, double);
+	static double getRhombusArea(int, int);
+
+	static int getCounter() { return counter; }
+
+	void menu();
+
+	~Area() = default;
+};
