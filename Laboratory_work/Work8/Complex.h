827b808844b71719d3612125196e11a118687147
SuvStreet <suvstreet@gmail.com>
2017-06-07 22:15:13 +0300

    Create Complex.h

---
+++ Laboratory_work/Work8/Complex.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<fstream>
+
+class Complex
+{
+	double real = 0;
+	double img = 0;
+public:
+	Complex() = default;
+	Complex(const double a, const double b) : real(a), img(b) {}
+	Complex(const Complex& myObj) : real(myObj.real), img(myObj.img) {}
+	Complex& operator=(const Complex&);
+
+	friend Complex operator+(const Complex&, const Complex&);
+	friend Complex operator-(const Complex&, const Complex&);
+	friend Complex operator*(const Complex&, const Complex&);
+	friend Complex operator/(const Complex&, const Complex&);
+
+	friend Complex& operator+=(Complex&, const Complex&);
+
+
+	friend std::ostream& operator<<(std::ostream& os, const Complex&);
+
+	~Complex() = default;
+};
