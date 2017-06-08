38f7dde3545947b411803ab5ebc888654a76ae2d
SuvStreet <suvstreet@gmail.com>
2017-06-08 16:57:36 +0300

    Create Complex.cpp

--- Laboratory_work/Work8/Complex.cpp
+++ Laboratory_work/Work8/Complex.cpp
@@ -31,20 +31,44 @@ Complex operator/(const Complex& com1, const Complex& com2) {
 
 Complex& operator+=(Complex& com1, const Complex& com2)
 {
-	com1.real += com2.real;
-	return com1;
+	double a1 = com1.real += com2.real;
+	double a2 = com1.img += com2.img;
+	return Complex(a1, a2);
+}
+
+Complex& operator-=(Complex& com1, const Complex& com2)
+{
+	double a1 = com1.real -= com2.real;
+	double a2 = com1.img -= com2.img;
+	return Complex(a1, a2);
+}
+
+Complex& Complex::operator *= (const Complex& com)
+{
+	real = ((real * com.real) - (img * com.img));
+	img = ((img * com.img) + (real * com.img));
+	return *this;
+
+	/*double z1 = ((com1.real * com2.real) - (com1.img * com2.img)) * ((com1.real * com2.real) - (com1.img * com2.img));
+	double z2 = ((com1.img * com2.img) + (com1.real * com2.img)) * ((com1.img * com2.img) + (com1.real * com2.img));*/
+
+	//double z3 = ;
+	//double z4 = ;
+
+	//z1 *= z3;
+	//z2 *= z4;
+
+	//return Complex(com1, com2);
 }
 
 std::ostream& operator<<(std::ostream& os, const Complex& com) {
 	os << com.real;
-	if (com.real > 0) { os << " + " << com.img; }
-	else { os << com.img << "i";  }
+	if (com.img >= 0) { os << "+" << com.img << "i"; }
+	else { os << com.img << "i"; }
 	return os;
 }
 
-
-//std::istream& operator >> (std::istream& is, Complex& a) {
-//	int real, img;
-//	is >> real >> img;
-//	return Complex(real, img);
-//}
+std::istream& operator >> (std::istream& is, Complex& com) {
+	is >> com.real >> com.img;
+	return is;
+}
