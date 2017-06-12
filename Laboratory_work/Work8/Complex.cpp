761475a9a61355505c422140b27ea84c11aeb645
SuvStreet <suvstreet@gmail.com>
2017-06-12 18:26:12 +0300

    Create Complex.cpp

--- Laboratory_work/Work8/Complex.cpp
+++ Laboratory_work/Work8/Complex.cpp
@@ -24,8 +24,11 @@ Complex operator*(const Complex& com1, const Complex& com2) {
 }
 
 Complex operator/(const Complex& com1, const Complex& com2) {
-	double q1 = (((com1.real * com2.real) + (com1.img * com2.img)) / (pow(com2.real, 2) + pow(com2.img, 2)));
-	double q2 = (((com2.real * com1.img) - (com1.real * com2.img)) / (pow(com2.real, 2) + pow(com2.img, 2)));
+	std::cout << com1.real << " " << com1.img;
+	double q1 = ((com1.real * com2.real) + (com1.img * com2.img)) / ((com2.real * com2.real) + (com2.img * com2.img));
+	std::cout << q1 << std::endl;
+	double q2 = ((com2.real * com1.img) - (com1.real * com2.img)) / ((com2.real * com2.real) + (com2.img * com2.img));
+	std::cout << q2 << std::endl;
 	return Complex(q1, q2);
 }
 
@@ -45,20 +48,12 @@ Complex& operator-=(Complex& com1, const Complex& com2)
 
 Complex& Complex::operator *= (const Complex& com)
 {
-	real = ((real * com.real) - (img * com.img));
-	img = ((img * com.img) + (real * com.img));
-	return *this;
+	return *this = *this * com;
+}
 
-	/*double z1 = ((com1.real * com2.real) - (com1.img * com2.img)) * ((com1.real * com2.real) - (com1.img * com2.img));
-	double z2 = ((com1.img * com2.img) + (com1.real * com2.img)) * ((com1.img * com2.img) + (com1.real * com2.img));*/
-
-	//double z3 = ;
-	//double z4 = ;
-
-	//z1 *= z3;
-	//z2 *= z4;
-
-	//return Complex(com1, com2);
+Complex& Complex::operator /= (const Complex& com)
+{
+	return *this = *this / com;
 }
 
 std::ostream& operator<<(std::ostream& os, const Complex& com) {
