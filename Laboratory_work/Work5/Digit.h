bbff3b224e9bb8099165635a40572c4e0f63b7b4
SuvStreet <suvstreet@gmail.com>
2017-06-19 23:33:55 +0300

    Create Digit.h

---
+++ Laboratory_work/Work5/Digit.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<fstream>
+
+class Digit
+{
+	int digit;
+	static size_t counter;
+public:
+	Digit();
+	explicit Digit(const int);
+	explicit Digit(const double);
+	~Digit() {}
+
+	Digit(const Digit&);
+	Digit(Digit&&);
+
+	int getDigit() const;
+	void setDigit(const int);
+	int getCounter() const;
+
+	Digit operator-();
+
+	Digit operator+(const Digit&);
+	Digit operator+(const int);
+
+	Digit operator-(const Digit&);
+	Digit operator-(const int);
+
+	Digit operator*(const Digit&);
+	Digit operator*(const int);
+
+	Digit operator/(const Digit&);
+	Digit operator/(const int);
+
+	Digit operator%(const Digit&);
+	Digit operator%(const int);
+
+	bool operator>(const Digit&);
+	bool operator>(const int);
+
+	bool operator<(const Digit&);
+	bool operator<(const int);
+
+	bool operator==(const Digit&);
+	bool operator==(const int);
+
+	bool operator!=(const Digit&);
+	bool operator!=(const int);
+
+	bool operator>=(const Digit&);
+	bool operator>=(const int);
+
+	bool operator<=(const Digit&);
+	bool operator<=(const int);
+
+	Digit& operator++();
+	Digit operator++(int);
+
+	Digit& operator--();
+	Digit operator--(int);
+
+	Digit& operator=(const Digit&);
+	Digit& operator=(Digit&&);
+};
