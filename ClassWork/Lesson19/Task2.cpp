8efe651e90113e8eae7304cdab579a7750963978
SuvStreet <suvstreet@gmail.com>
2017-06-21 23:17:01 +0300

    Update Task2.cpp

--- ClassWork/Lesson19/Task2.cpp
+++ ClassWork/Lesson19/Task2.cpp
@@ -24,7 +24,51 @@ int main()
 	x = sum / lis.size();
 	lis.pop_front();
 	std::cout << x << std::endl;
+	// 3) Вставили в начало списка число, равное целой части среднего арифметического всех элементов списка.
 
+	for (auto it = lis.begin(); it != lis.end(); ) {
+		if ((*it) == 130) {
+			it = lis.erase(it);
+			it++;
+		}
+		else { ++it; }
+	}
+	for (auto& number : lis) {
+		std::cout << number << " ";
+	}
+	std::cout << std::endl;
+	// 4) Удалили из списка число 130
+
+	size_t index = 1;
+	for (auto it = lis.begin(); it != lis.end(); ++it){
+		if (index == 10){
+			(*it *= 3);
+		}
+		++index;
+	}
+	for (auto& number : lis) {
+		std::cout << number << " ";
+	}
+	std::cout << std::endl;
+	// 5) Увеличили 10-е число в 3 раза
+	
+	lis.pop_back();
+	for (auto& number : lis) {
+		std::cout << number << " ";
+	}
+	std::cout << std::endl;
+	// 6) Удалили из списка последнее число 
+
+	/*for (auto it = lis.begin(); it != lis.end(); ++it){
+
+	}*/
+
+	lis.push_back(70);
+	for (auto& number : lis) {
+		std::cout << number << " ";
+	}
+	std::cout << std::endl;
+	// 9) Вставили в конец списка число 70
 
 	std::cout << std::endl;
 	return 0;
