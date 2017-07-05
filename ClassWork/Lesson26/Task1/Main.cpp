b10ef9b2dbf684d33cef1125062eeb50cab33d70
SuvStreet <suvstreet@gmail.com>
2017-07-05 22:17:32 +0300

    Create Main.cpp

---
+++ ClassWork/Lesson26/Task1/Main.cpp
@@ -0,0 +1,8 @@
+#include "System.h"
+
+int main() {
+	System system;
+	std::cout << system.getValueCoreSystem() << std::endl;
+	std::cout << system.getValueStaticCoreSystem() << std::endl;
+	return 0;
+}
