44f1569de392dc4b8530f9142a2432cb8c9d22ba
SuvStreet <suvstreet@gmail.com>
2017-07-17 18:26:59 +0300

    Update Kolotysha_Circle.h

--- Laboratory_work/Work21/Task1/Kolotysha_Circle.h
+++ Laboratory_work/Work21/Task1/Kolotysha_Circle.h
@@ -4,15 +4,19 @@
 class Circle : public Shape
 {
 	double radius = 0;
-	double centerX_Y = 0;
+	double centerX = 0;
+	double centerY = 0;
 public:
-	Circle(const double, const double);
+	Circle(const double, const double, const double);
 
 	double getSideA() const;
 	void setSideA(const double);
 
-	double getCenterX_Y() const;
-	void setCenterX_Y(const double);
+	double getCenterX() const;
+	void setCenterX(const double);
+
+	double getCenterY() const;
+	void setCenterY(const double);
 
 	double getArea();
 
