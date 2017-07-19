d482909592c19c8aa60acd3b5c8d16411a0a95e3
SuvStreet <suvstreet@gmail.com>
2017-07-19 21:36:57 +0300

    Update PrettyPrint.h

--- Laboratory_work/Work1/Task3/PrettyPrint.h
+++ Laboratory_work/Work1/Task3/PrettyPrint.h
@@ -1,12 +1,11 @@
 #pragma once
-#include<iostream>
-#include<string>
+#include <string>
 
 class PrettyPrint
 {
 public:
-	PrettyPrint() {};
-	std::string concatenation(std::string, std::string) const;
-	std::string substitution(std::string);
-	void coup(std::string*);
+	static std::string concatenate(std::string&, std::string&);
+	static std::string replace(std::string&);
+	static std::string coup(std::string&);
+	~PrettyPrint() {}
 };
