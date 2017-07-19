2b4d3676b657821e3866d4e3a585325e3afb1a64
SuvStreet <suvstreet@gmail.com>
2017-07-19 21:36:25 +0300

    Update Main.cpp

--- Laboratory_work/Work1/Task3/Main.cpp
+++ Laboratory_work/Work1/Task3/Main.cpp
@@ -1,22 +1,23 @@
-#include"PrettyPrint.h"
+#include "PrettyPrint.h"
+#include <iostream>
 
 int main()
 {
-	std::string a = "file", b = "write", c = "appearance", d = "Madam I'm Adam";
+	setlocale(LC_ALL, "Russian");
+	std::string a = "file", b = "write", c = "АБЖа_AppearAnce", d = "Madam I'm Adam";
 	PrettyPrint x;
 
 	std::cout << "Returning a string is a concatenation of two lines with a point between them." << std::endl;
 	std::cout << "Before: " << a + " " + b << std::endl;
-	std::cout << "After: " << x.concatenation(a, b) << std::endl << std::endl;
+	std::cout << "After:  " << x.concatenate(a, b) << std::endl << std::endl;
 
 	std::cout << "Replacing all letters a with o:" << std::endl;
 	std::cout << "Before: " << c << std::endl;
-	std::cout << "After: " << x.substitution(c) << std::endl << std::endl;
+	std::cout << "After: " << x.replace(c) << std::endl << std::endl;
 
 	std::cout << "Words palindromes:" << std::endl;
 	std::cout << "Before: " << d << std::endl;
-	x.coup(&d);
-	std::cout << "After: " << d << std::endl << std::endl;
+	std::cout << "After: " << x.coup(d) << std::endl << std::endl;
 
 	return 0;
 }
