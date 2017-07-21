5381f3cf4ced7868aa7f6b3826bc18d19c2f911e
SuvStreet <suvstreet@gmail.com>
2017-07-21 13:57:02 +0300

    Delete Main.cpp

--- ClassWork/Lesson26/Task1/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "System.h"
-
-int main() {
-	System system;
-	std::cout << system.getValueCoreSystem() << std::endl;
-	std::cout << system.getValueStaticCoreSystem() << std::endl;
-	return 0;
-}
