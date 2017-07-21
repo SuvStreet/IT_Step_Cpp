4b4f2a2adb04a618e0a98f0e483af6a8047b765d
SuvStreet <suvstreet@gmail.com>
2017-07-21 13:57:07 +0300

    Delete Sustem.cpp

--- ClassWork/Lesson26/Task1/Sustem.cpp
+++
@@ -1,9 +0,0 @@
-#include"System.h"
-
-std::string System::Core::getPrintCore() { return "Hello, I class Core"; }
-size_t System::Core::setSystemCore() { return core; }
-void System::Core::getValueCore(const size_t value) { core = value; }
-
-std::string System::printSystem() { return "Hello, I class System"; }
-std::string System::setSystem() { return system; }
-void System::getValueSystem(const std::string value) { system = value; }
